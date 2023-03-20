4c2f42c40af03ab1def76b98aa3417e6a81c09da
Regen1337 <regennukes@yahoo.com>
2023-03-20 14:49:57 -0400

    .

--- gamemode/core/modules/characters/net/cl_net.lua
+++ gamemode/core/modules/characters/net/cl_net.lua
@@ -4,11 +4,39 @@
     @param idx The index of the variable
     @param val The value of the variable
 ]==]
-net.Receive("netScape.character.vars.sync", function()
+net.Receive("netScape.character.var.sync", function()
     local sender = net.ReadEntity()
     local id = net.ReadUInt(8)
     local idx = net.ReadString()
     local val = net.ReadType()
     
-    receiver:
+    local character = sender:getCharacterSlot(id) or gScape.core.character.default.vars
+    character.vars[idx] = val
+    sender:setCharacter(character.vars)
+    sender:setCharacterSlot(id, character)
+end)
+
+--[==[
+    @param receiver The entity to receive the data
+    @param id The character ID
+    @param tbl The table of variables
+]==]
+net.Receive("netScape.character.vars.sync", function()
+    local sender = net.ReadEntity()
+    local id = net.ReadUInt(8)
+    local tbl = net.ReadTable()
+    
+    sender:setCharacter(tbl.vars)
+    sender:setCharacterSlot(id, tbl)
+end)
+
+--[==[
+    @param receiver The entity to receive the data
+    @param tbl The table of variables
+]==]
+net.Receive("netScape.characters.vars.sync", function()
+    local sender = net.ReadEntity()
+    local tbl = net.ReadTable()
+    
+    sender:setCharacters(tbl)
 end)
